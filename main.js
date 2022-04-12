98e05d7a1206e9dfa9d4c3253fc7748d0db2402f
felixnaredi <naredi@student.chalmers.se>
2022-04-12 15:24:59 +0200

    deploy

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{const o=document.getElementById("email-suggestion-list");["john.doe@gmail.com","doe.john@gmai.com","john.alex.doe@gmail.com","john.doe1@gmail.com","john.doe2@gmail.com","john.doe3@gmail.com","doe.john.alex@gmail.com"].forEach((e=>{const m=document.createElement("li");m.innerHTML=e,o.appendChild(m)}))})();
